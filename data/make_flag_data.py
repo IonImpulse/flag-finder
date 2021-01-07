44887f12595442b93707db09c179f9ffe4c4dea7
Ethan Vazquez <edv121@outlook.com>
2021-01-07 11:20:54 -0800

    finished demo/start database

---
+++ data/make_flag_data.py
@@ -0,0 +1,29 @@
+import csv
+import json
+
+codes = open("codes.json")
+codes = json.load(codes)
+
+codes = {value.replace(" ", "-"): key for key, value in codes.items()}
+
+print(codes)
+
+with open("flag.data.csv", "r", newline='', encoding="utf8") as f :
+    flag_data = [row for row in csv.reader(f, delimiter = ',')]
+
+print(flag_data)
+
+out_data = []
+
+for row in flag_data :
+    if row[0] in codes :
+        out_data.append(row)
+        out_data[-1][1] = f"https://flagcdn.com/w640/{codes[row[0]]}.png"
+
+    else :
+        out_data.append(row)
+        print("Couldn't do", row)
+
+with open("flag.database.csv", "w", encoding="utf8", newline="") as f :
+    csv_writer = csv.writer(f, dialect="excel")
+    csv_writer.writerows(out_data)
